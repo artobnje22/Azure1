1801ea444991437ef718aed7f0fd0fab33b76602
artobnje22 <70949554+artobnje22@users.noreply.github.com>
2020-09-11 12:04:38 +0530

    Update HomeController.cs
    updated azure

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Aritro's Page";
+            ViewBag.Message = "Aritro's Page updated";
 
             return View();
         }
